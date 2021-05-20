d400584be5fd194a06d3657fa2619f457fb88105
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2021-05-21 03:52:32 +0500

    v1.2 - Changed from mobile API to Web

--- vu.py
+++ vu.py
@@ -1,151 +1,162 @@
-from discord.ext import commands
-from time import sleep
+from bs4 import BeautifulSoup
 
 import requests
-import json
+import urllib3
 import datetime, pytz
-import discord
+import json
+import re
 
-def login(loginEndpoint, username, password):
-    response        = requests.post(loginEndpoint,
-        headers     = {
-            "User-Agent": "Mozilla/5.0",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Connection": "close",
-            "Accept-Encoding": "gzip, deflate"
-        },
-        data        = {
-            "studentid": username,
-            "appKey": password,
-            "fcm_token": "null",
-            "device_id": "123456"
-        }
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+def fixAndReturnDates(start, end):
+    start       = datetime.datetime.strptime("-".join(start.split(",")[::-1]), '%d-%m-%Y').strftime("%d-%m-%Y")
+    end         = (datetime.datetime.strptime("-".join(end.split(",")[::-1]), '%d-%m-%Y') - datetime.timedelta(days=1)).strftime("%d-%m-%Y")
+    dateToday   = datetime.datetime.now(pytz.timezone('Asia/Karachi')).strftime("%d-%m-%Y")
+
+    # print(start, end, dateToday)
+    return(start, end, dateToday)
+
+def returnRequestDetailsOnFailure(url, customString, requestObj):
+    string      =  f"\n[!] {customString}: {url}\n"
+    string      += f"[!] Status Code: {requestObj.status_code}\n"
+    string      += f"[!] Response Headers: {requestObj.headers.items()}\n"
+    string      += f"[!] Some text of the body: {requestObj.text[:200]}\n"
+    print(string)
+
+def loginIntoWebApplication(studentId, password):
+    loginURL    = "https://vulms.vu.edu.pk:443/LMS_LP.aspx"
+
+    session             = requests.session()
+    getLoginParameters  = session.get(loginURL,
+        # proxies = {
+        #     'http': '127.0.0.1:8080',
+        #     'https': '127.0.0.1:8080',
+        # },
+        verify  = False,
     )
 
-    if response.status_code == 200:
-        print("[#] Logged in successfully!\n")
+    if getLoginParameters.status_code == 200:
+        soup        = BeautifulSoup(getLoginParameters.text, 'html.parser')
+        viewstate   = soup.find_all('input', {'id': '__VIEWSTATE'})[0]['value']
+        eventvalid  = soup.find_all('input', {'id': '__EVENTVALIDATION'})[0]['value']
 
-        responseTxt = response.text
-        jsonData    = json.loads(responseTxt)
-        jsonFields  = jsonData['All'][0]
+        # Login form won't work without these params
 
-        NAME        = jsonFields['Name']
-        AccessToken = jsonFields['AccessToken']
+        print(f"[%] __VIEWSTATE: {viewstate}")
+        print(f"[%] __EVENTVALIDATION: {eventvalid}")
 
-        return (NAME, AccessToken)
+        # Loggin in with the viewstate and eventvalidation parameters. 
+
+        login       = session.post(loginURL,
+            # proxies = {
+            #     'http': '127.0.0.1:8080',
+            #     'https': '127.0.0.1:8080',
+            # },
+            verify  = False,
+            data    = {
+                "__VIEWSTATE": viewstate,
+                "__EVENTVALIDATION": eventvalid,
+                "txtStudentID": studentId,
+                "txtPassword": password,
+                "cbKeepMeLogin": "on",
+                "ibtnLogin": "Sign In"
+            }
+        )
+
+        if login.status_code == 200:
+            cookies     = dict(login.cookies)
+
+            if 'stdUserName' in cookies:
+                print(f"\n[#] User ({cookies['stdUserName']}) logged in!")
+
+                # Need to give /Home.aspx a hit with some token without that, the cookies don't work lol! (found after hella debuggin)
+                locationHref    = re.findall(r'window\.top\.location\.href=\'Home\.aspx\?id\=(.*?)\'\;', login.text)[0]
+
+                homePath        = f'https://vulms.vu.edu.pk/Home.aspx?id={locationHref}'
+                homeRequest     = session.get(homePath,
+                    # proxies = {
+                    #     'http': '127.0.0.1:8080',
+                    #     'https': '127.0.0.1:8080',
+                    # },
+                    verify  = False,
+                )
+
+                return(session)
+
+        else:
+            returnRequestDetailsOnFailure(url = loginURL, customString = "There was an error trying to fetch params", requestObj = getLoginParameters)
 
     else:
-        print("[!] Login failed!")
+        returnRequestDetailsOnFailure(url = loginURL, customString = "There was an error trying to login", requestObj = login)
 
-def fetchGDB(gdbEndpoint, accessToken):
-    response        = requests.post(gdbEndpoint,
-        headers     = {
-            "User-Agent": "Mozilla/5.0",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Connection": "close",
-            "Accept-Encoding": "gzip, deflate"
-        },
-        data        = {
-            "accessToken": accessToken
-        }
+def fetchCalendarAndDetails(session):
+    calendarURL     = 'https://vulms.vu.edu.pk/ActivityCalendar/ActivityCalendar.aspx'
+    request         = session.get(calendarURL,
+        # proxies = {
+        #     'http': '127.0.0.1:8080',
+        #     'https': '127.0.0.1:8080',
+        # },
+        verify  = False,
     )
 
-    if response.status_code == 200:
-        gdbJson     = json.loads(response.text)['All']
-        fOutput     = ""
-
-        print("[&] GDBs:\n")
+    if request.status_code == 200:
+        post            = ""
+        source          = request.text
+        jsonData        = re.findall(r'var\sJsonData\s\=\s(.*?)\;', source)[0]
         
-        for gdbs in gdbJson[:3]:
-            crs     = gdbs['CourseCode']
-            sDate   = int(gdbs['StartDate'].replace('/Date(', '').replace(')/', '')[:-3])
-            eDate   = int(gdbs['EndDate'].replace('/Date(', '').replace(')/', '')[:-3])
+        calendarJSON    = json.loads(jsonData)
+        print(json.dumps(calendarJSON, indent=4))
+        print()
 
-            stDate  = datetime.datetime.fromtimestamp(sDate, pytz.timezone('Asia/Karachi')).strftime('%d-%m-%Y')
-            enDate  = datetime.datetime.fromtimestamp(eDate, pytz.timezone('Asia/Karachi')).strftime('%d-%m-%Y')
-            dToday  = datetime.datetime.now(pytz.timezone('Asia/Karachi')).strftime("%d-%m-%Y")
+        for subjects in calendarJSON:
+            coursecode  = subjects['coursecode']
+            title       = subjects['title']
+            url         = subjects['url']
+            start       = subjects['start']
+            end         = subjects['end']
 
-            if stDate == dToday:
-                stDate += " **(today)**"
+            startDate, endDate, dateToday   = fixAndReturnDates(start, end)
 
-            if enDate == dToday:
-                enDate += " __**(today)**__"
+            if startDate == dateToday:
+                startDate += " **(today)**"
 
-            fOutput += f"[#] Course: {crs}\nStart Date: {stDate}\nEnd Date: {enDate}\n\n"
+            if endDate == dateToday:
+                endDate   += " __**(today)**__"  
 
-        print(fOutput)
-        return(fOutput)
+            post        += f"[#] **{title}**\n"
+            post        += f"Start date: {startDate}\n"
+            post        += f"End date: {endDate}\n\n"
 
-    else:
-        print("[!] Can't load GDB endpoint..")
-
-def fetchQuizzes(quizEndpoint, accessToken):
-    response        = requests.post(quizEndpoint,
-        headers     = {
-            "User-Agent": "Mozilla/5.0",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "Connection": "close",
-            "Accept-Encoding": "gzip, deflate"
-        },
-        data        = {
-            "accessToken": accessToken
-        }
-    )
-
-    if response.status_code == 200:
-        fOutput     = ""
-        quizJson    = json.loads(response.text)['All']
-
-        print("[&] Quizzes:\n")
-        
-        for quizzes in quizJson[:3]:
-            crs     = quizzes['courseCode']
-            title   = quizzes['Title']
-            sDate   = int(quizzes['StartDate'].replace('/Date(', '').replace(')/', '')[:-3])
-            eDate   = int(quizzes['EndDate'].replace('/Date(', '').replace(')/', '')[:-3])
-
-            stDate  = datetime.datetime.fromtimestamp(sDate, pytz.timezone('Asia/Karachi')).strftime('%d-%m-%Y')
-            enDate  = datetime.datetime.fromtimestamp(eDate, pytz.timezone('Asia/Karachi')).strftime('%d-%m-%Y')
-            dToday  = datetime.datetime.now(pytz.timezone('Asia/Karachi')).strftime("%d-%m-%Y")
-
-            if stDate == dToday:
-                stDate += " **(today)**"
-
-            if enDate == dToday:
-                enDate += " __**(today)**__"
-
-            fOutput += f"[#] Course: {crs}\nTitle: {title}\nStart Date: {stDate}\nEnd Date: {enDate}\n\n"
-
-        print(fOutput)
-        return(fOutput)
+        print(post)
+        return(post)
 
     else:
-        print("[!] Can't load GDB endpoint..")
+        returnRequestDetailsOnFailure(url = calendarURL, customString = "There was an error trying to fetch calendar", requestObj = request)
+
+def postIntoDiscord(post, webHookURL):
+    discordPost     = requests.post(webHookURL, {
+        "content": post
+    })
+
+    if discordPost.status_code == 204:
+        print("[#] Posted in discord!")
+
+    else:
+        returnRequestDetailsOnFailure(url = webHookURL[:10], customString = "There was an error trying to post on webhook", requestObj = discordPost)
 
 def main():
-    username        = "bcXXXXXX"       # VU User's ID
-    password        = "XXXXXXXX"       # VU User's Password
+    with open('config.json', 'r') as f: configContents = json.loads(f.read().strip())
 
-    loginEndpoint   = "https://ws.vu.edu.pk/MobileApp/Student.asmx/GetStudent"
-    gdbEndpoint     = "https://ws.vu.edu.pk/MobileApp/GradedActivitiesToDo.asmx/getGDB"
-    quizEndpoint    = "https://ws.vu.edu.pk/MobileApp/GradedActivitiesToDo.asmx/getQuizzes"
-    assignEndpoint  = "https://ws.vu.edu.pk/MobileApp/GradedActivitiesToDo.asmx/getAssignments"
+    studentId   = configContents['studentId']
+    password    = configContents['password']
+    webHookURL  = configContents['discordWebHookURL']
 
-    webHookURL      = "https://discord.com/api/webhooks/XXX/XXX"
+    print("[&] Logging into the Web application...\n")
 
-    while True:
-        name, token     = login(loginEndpoint, username, password)
-        gdbs            = fetchGDB(gdbEndpoint, token)
-        quizzes         = fetchQuizzes(quizEndpoint, token)
+    session     = loginIntoWebApplication(studentId, password)
+    post        = fetchCalendarAndDetails(session)
 
-        requests.post(webHookURL, {
-            "content": f"[&] GDBs: \n\n{gdbs}--\n\n[&] Quizzes: \n\n{quizzes}"
-        })
-
-        sleep(21600)    # 6 Hours of time -- to post updates
+    postIntoDiscord(post, webHookURL)
 
 if __name__ == '__main__':
     main()
