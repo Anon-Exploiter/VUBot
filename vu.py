ac892305a970c13d5960dce025cf655b5bcfa7d2
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2021-08-20 22:42:29 +0500

    Just check one env variable

--- vu.py
+++ vu.py
@@ -270,11 +270,7 @@ def postIntoDiscord(post, webHookURL):
 
 
 def main():
-    if (
-        os.getenv("VUSERNAME")
-        and os.getenv("VPASSWORD")
-        and os.getenv("WEBHOOK_URL")
-    ):
+    if os.getenv("VUSERNAME"):
         studentId = os.getenv("VUSERNAME")
         password = os.getenv("VPASSWORD")
         webHookURL = os.getenv("WEBHOOK_URL")
