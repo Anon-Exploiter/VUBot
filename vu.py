737cbc7053d56322689a27a338d31a17a2c45667
Syed Umar Arfeen <18597330+Anon-Exploiter@users.noreply.github.com>
2021-08-20 22:30:41 +0500

    $USERNAME defaults to local user - This fixes it

--- vu.py
+++ vu.py
@@ -262,9 +262,14 @@ def postIntoDiscord(post, webHookURL):
 
 
 def main():
-    studentId = os.getenv("USERNAME")
-    password = os.getenv("PASSWORD")
-    webHookURL = os.getenv("WEBHOOK_URL")
+    if (
+        os.getenv("VUSERNAME")
+        and os.getenv("VPASSWORD")
+        and os.getenv("WEBHOOK_URL")
+    ):
+        studentId = os.getenv("VUSERNAME")
+        password = os.getenv("VPASSWORD")
+        webHookURL = os.getenv("WEBHOOK_URL")
 
     print("[&] Logging into the Web application...\n")
 
